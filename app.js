c2758369220bab1fdf744268ef66d070ad1529b7
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-06-30 01:15:44 +0000

    added function to generate random short URL

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const http = require("http");
 const PORT = 8080;
 
@@ -12,6 +14,19 @@ function requestHandler(request, response) {
     response.end("Unknown Path");
   }
 }
+
+function generateRandomShortURL(length) {
+  var results = " "
+  var possibilities = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+  for (var i = 0; i <= 5; i++) {
+    results += possibilities.charAt(Math.floor(Math.random() *  possibilities.length));
+  }
+  return results;
+}
+
+// generateRandomString(6);
+
 
 var server = http.createServer(requestHandler);
 
--- express_server.js
+++ express_server.js
@@ -3,11 +3,22 @@ var express = require("express");
 var app = express();
 app.set("view engine", "ejs");
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.end("Hello!");
