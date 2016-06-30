a7e1146deb3c64ebc5dd6bd5792213b3e522d2ac
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-06-30 01:50:05 +0000

    Fixed urls index page and put link on urls show page

--- express_server.js
+++ express_server.js
@@ -30,7 +30,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase};
   res.render("urls_show", templateVars);
 });
 
